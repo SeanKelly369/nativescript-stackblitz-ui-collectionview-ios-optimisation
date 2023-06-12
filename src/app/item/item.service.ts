de08d95232c63e3177dbe613cceb0a0bd2f9fd03
SeanKelly369 <36159246+SeanKelly369@users.noreply.github.com>
2023-06-12 22:08:54 +0100

    Creating table data

--- src/app/item/item.service.ts
+++ src/app/item/item.service.ts
@@ -7,18 +7,18 @@ import { Item } from './item'
 })
 export class ItemService {
   private items = new Array<Item>(
-    { id: 1, name: 'Ter Stegen', role: 'Goalkeeper' },
-    { id: 3, name: 'Piqué', role: 'Defender' },
-    { id: 4, name: 'I. Rakitic', role: 'Midfielder' },
-    { id: 5, name: 'Sergio', role: 'Midfielder' },
-    { id: 6, name: 'Denis Suárez', role: 'Midfielder' },
-    { id: 7, name: 'Arda', role: 'Midfielder' },
-    { id: 8, name: 'A. Iniesta', role: 'Midfielder' },
-    { id: 9, name: 'Suárez', role: 'Forward' },
-    { id: 10, name: 'Messi', role: 'Forward' },
-    { id: 11, name: 'Neymar', role: 'Forward' },
-    { id: 12, name: 'Rafinha', role: 'Midfielder' },
-    { id: 13, name: 'Cillessen', role: 'Goalkeeper' },
+    { id: 1, name: 'Hugo Lloris', role: 'Goalkeeper', height: 1.88, nationality: 'French', dob: '26/12/1986'  },
+    { id: 3, name: 'Oliver Skipp', role: 'Midfielder', height: 1.75, nationality: 'English', dob: '16/09//2000' },
+    { id: 4, name: 'Dejan Kulusevski', role: 'Midfielder/Winger', height: 1.86, nationality: 'Sweden', dob:  '25/04/2000'},
+    { id: 5, name: 'Pierre-Emile Højbjerg', role: 'Midfielder', height: 1.87, nationality: 'Denmark', dob: '05/08/1995' },
+    { id: 6, name: 'Pedro Porro', role: 'Right Back', height: 1.73, nationality: 'Spanish', dob: '13/09/1999' },
+    { id: 7, name: 'Davinson Sánchez', role: 'Defender', height: 1.87, nationality: 'Columbian', dob: '12/06/1996' },
+    { id: 8, name: 'Japhet Tanganga', role: 'Defender', height: 1.84, nationality: 'English', dob: '31/03/1999' },
+    { id: 9, name: 'Son Heung-min', role: 'Forward', height: 1.83, nationality: 'South Korean', dob: '08/07/1992' },
+    { id: 10, name: 'Lucas Moura', role: 'Forward', height: 1.72, nationality: 'Brazilian', dob: '13/08/1992' },
+    { id: 11, name: 'Richarlison', role: 'Forward', height: 1.84, nationality: 'Brazilian', dob: '10/05/1997' },
+    { id: 12, name: 'Pape Matar Sarr', role: 'Midfielder', height: 1.85, nationality: 'Senegal', dob: '14/09/2002' },
+    { id: 13, name: 'Harry Kane', role: 'Forward', height: 1.88, nationality: 'English', dob: '28/07/1993' },
     { id: 14, name: 'Mascherano', role: 'Defender' },
     { id: 17, name: 'Paco Alcácer', role: 'Forward' },
     { id: 18, name: 'Jordi Alba', role: 'Defender' },
@@ -28,7 +28,7 @@ export class ItemService {
     { id: 22, name: 'Aleix Vidal', role: 'Midfielder' },
     { id: 23, name: 'Umtiti', role: 'Defender' },
     { id: 24, name: 'Mathieu', role: 'Defender' },
-    { id: 25, name: 'Masip', role: 'Goalkeeper' }
+    { id: 25, name: 'Fraser Forster', role: 'Goalkeeper', height: 2.01, nationality: 'English', dob: '17/03/1988' }
   )
 
   getItems(): Array<Item> {
--- src/app/item/item.ts
+++ src/app/item/item.ts
@@ -2,4 +2,7 @@ export interface Item {
   id: number
   name: string
   role: string
+  height: number
+  nationality: string
+  dob: string
 }
